faf3554e42c546270a49359ddabc47c833afa3d1
steveraspi <stvnchnsn@gmail.com>
2021-02-24 23:40:21 -0600

    added Pinky bot and made Robot a sublcass of Player and added inputs for robots

--- players.py
+++ players.py
@@ -1,30 +1,94 @@
 import random
 
 def test_main():
-    steve = Human('Steve')
-    steve.choice()
+    steve = Random_Bot('Steve')
+    d = steve.decision()
+    steve.return_num_of_rounds_played()
+    print(d)
 
-protocol = test_main
+def test_memory_bot_v1():
+    pinky = Pinky("Pinky")
+    d = pinky.decision()
+    print(d)
+
+protocol = test_memory_bot_v1
 
 class Player:
     def __init__(self,name):
         self.playername = name
         self.points = 0
+        self.num_of_rounds_played = 0
+        self.selection = None
     def name(self):
         return self.playername
     def point(self,amount = 1):
-        self.points +=1
+        self.points += amount
     def score(self):
         return self.points
+    def return_num_of_rounds_played(self):
+        print(self.num_of_rounds_played)
+        return self.num_of_rounds_played
+    def decision(self):
+        '''different for human or robot, so this method is overruled at the subclass level'''
+        return self.selection
+    def tell_results(self,result,opponent_choice):
+        ''' Route the result to the robot'''
+        pass
+    
+    
 class Human(Player):
-    def choice(self):
-        return input("Rock(r), Paper(p) or Sissors(s)?: ")
+    def decision(self):
+        self.selection = input("Rock(r), Paper(p) or Sissors(s)?: ")
+        try:
+            assert self.selection in ['r','p','s'],"invalid selection chooose 'r','p' or 's'"
+        except AssertionError:
+            error = True
+            while error:
+                self.selection = input("Rock(r), Paper(p) or Sissors(s)?: ")
+                try:
+                    assert self.selection in ['r','p','s']
+                    error = False
+                except AssertionError:
+                    "invalid selection chooose 'r','p' or 's'"
+        self.num_of_rounds_played +=1
+        return self.selection
 
 class Robot(Player):
-    def choice(self):
-        hand_dict = {1:'r',2:'p',3:'s'}
-        return hand_dict[random.randint(1,3)]
+    def __init__(self,name):
+        super().__init__(name)
+        self.hand_dict = {1:'r',2:'p',3:'s'}
+        self.last_result = None
+    def rand_1(self):
+        self.selection = self.hand_dict[random.randint(1,3)]
 
+
+class Random_Bot(Robot):
+    '''returns random selection no matter what'''
+    def decision(self):
+        self.rand_1()
+        self.num_of_rounds_played +=1
+        return self.selection
+class Pinky(Robot):
+    def decision(self):
+        self.num_of_rounds_played +=1
+        if self.num_of_rounds_played == 1:
+            self.rand_1()
+            return self.selection
+        else:
+            if self.last_result == 0:
+                '''play random'''
+                self.rand_1()
+                return self.selection
+            if self.last_result == 1:
+                '''play same'''
+                return self.selection
+            if self.last_result == -1:
+                '''play oppoenets choice'''
+                return self.last_opposing_choice
+        
+    def tell_results(self,result,opponent_choice):
+        self.last_result = result
+        self.last_opposing_choice = opponent_choice
 
 if __name__ == '__main__':
     protocol()
--- round.py
+++ round.py
@@ -1,37 +1,81 @@
 import time
 from players import Human
-from players import Robot
+from players import Random_Bot
+from players import Pinky
 
-def test_main():
-    round = Round("Steve","Asmov")
-    round.count_down()
-    round.hand_analyzer()
-protocol = test_main
+def battle_of_the_bots():
+    roy = Random_Bot('Roy')
+    asimov = Random_Bot('Asimov')
+    choices = []
+    for i in range(5):
+        round = Round(roy,asimov)
+        #round.count_down()
+        round.players_choice()
+        round.hand_analyzer()
+        choices.append(round.results())
+    print("{}'s score: ".format(roy.name()),roy.score())
+    print("{}'s score: ".format(asimov.name()),asimov.score())
+    print(choices)
+
+
+
+def human_v_robot(robot):
+    human_name = input("What is human's name?: ")
+    human = Human(human_name)
+    
+    choices = []
+    for i in range(5):
+        round = Round(human,robot)
+        round.count_down()
+        round.players_choice()
+        round.hand_analyzer()
+        print(robot.return_num_of_rounds_played())
+        choices.append(round.results())
+    print("{}'s score: ".format(human.name()),human.score())
+    print("{}'s score: ".format(robot.name()),robot.score())
+    print(choices)
+
+protocol = human_v_robot
+asimov = Random_Bot('Asimov')
+pinky = Pinky('Pinky')
+robot = pinky
 
 class Round:
-    def __init__(self,human,robot):
-        self.human = Human(human)
-        self.robot = Robot(robot)
+    def __init__(self,Player1,Player2):
+        self.player1 = Player1
+        self.player2 = Player2
+ 
     def count_down(self):
         for i in range(1,4):
             print(i)
             time.sleep(1)
-        self.human_choice = self.human.choice()
-        self.robot_choice = self.robot.choice()
+    def players_choice(self):
+        self.player1_choice= self.player1.decision()
+        self.player2_choice = self.player2.decision()
+    def results(self):
+        return  {self.player1.name():self.player1_choice,
+        self.player2.name():self.player2_choice}
     def hand_analyzer(self):
         hand_key = {'r':'Rock','p':'Paper','s':"Sissors"}
-        print("{}'s choice: ".format(self.human.name()),hand_key[self.human_choice])
-        print("{}'s choice: ".format(self.robot.name()),hand_key[self.robot_choice])
+        print("{}'s choice: ".format(self.player1.name()),hand_key[self.player1_choice])
+        print("{}'s choice: ".format(self.player2.name()),hand_key[self.player2_choice])
         winners = {'r':'s','p':'r','s':'p'}
-        if self.human_choice == self.robot_choice:
+        if self.player1_choice == self.player2_choice:
+            play1_results = 0
+            play2_results = 0
             print('tie!')
-        elif winners[self.human_choice] == self.robot_choice:
-            self.human.point()
-            print('{} Wins!'.format(self.human.name()))
+        elif winners[self.player1_choice] == self.player2_choice:
+            self.player1.point()
+            play1_results = 1
+            play2_results = -1
+            print('{} Wins!'.format(self.player1.name()))
         else:
-            self.robot.point()
-            print('{} Wins!'.format(self.robot.name()))
+            self.player2.point()
+            play1_results = -1
+            play2_results = 1
+            print('{} Wins!'.format(self.player2.name()))
+        self.player1.tell_results(play1_results,self.player2_choice)
+        self.player2.tell_results(play2_results,self.player1_choice)
         
-
 if __name__ =='__main__':
-    protocol()
+    protocol(robot)
